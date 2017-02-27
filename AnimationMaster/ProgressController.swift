43c53505a43ca2e635327cc721aebdc160e796dc
我是五高你敢信 <745170149@qq.com>
2017-02-27 19:54:00 +0800

    Prepare ProgressView

---
+++ AnimationMaster/ProgressController.swift
@@ -0,0 +1,19 @@
+//
+//  ProgressController.swift
+//  AnimationMaster
+//
+//  Created by 我是五高你敢信 on 2017/2/27.
+//  Copyright © 2017年 我是五高你敢信. All rights reserved.
+//
+
+import UIKit
+
+class ProgressController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+}
---
+++ AnimationMaster/ProgressView.swift
@@ -0,0 +1,21 @@
+//
+//  ProgressView.swift
+//  AnimationMaster
+//
+//  Created by 我是五高你敢信 on 2017/2/27.
+//  Copyright © 2017年 我是五高你敢信. All rights reserved.
+//
+
+import UIKit
+
+class ProgressView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
