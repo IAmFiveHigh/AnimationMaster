26dd5dc01cd7f078f6c82212abd14c0a6aab4741
IAmFiveHigh <745170149@qq.com>
2017-11-17 15:40:53 +0800

    旋转loading动画

--- AnimationMaster/旋转loding图/旋转loading图Controller.swift
+++ AnimationMaster/旋转loding图/旋转loading图Controller.swift
@@ -10,90 +10,69 @@ import UIKit
 
 class 旋转loading图Controller: BaseViewController {
 
-    enum RotationType {
-        case 张开
-        case 收缩
-    }
-    private var type: RotationType = .张开
+    var sharp: CAShapeLayer!
+    var sharp2: CAShapeLayer!
     
-    private let mainView = UIView()
-    private var cycle1: CircleView!
-    private var cycle2: CircleView!
-    
-    private var timer: Timer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        mainView.frame = CGRect(origin: view.center, size: CGSize(width: 200, height: 200))
+        let mainView = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        mainView.backgroundColor = UIColor.black
         mainView.center = view.center
-        mainView.backgroundColor = #colorLiteral(red: 0.06274510175, green: 0, blue: 0.1921568662, alpha: 1)
         view.addSubview(mainView)
-        
-        
-        cycle1 = CircleView(frame: CGRect.zero, lineWidth: 2, lineColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), clockWise: true)
-        cycle2 = CircleView(frame: CGRect.zero, lineWidth: 2, lineColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), clockWise: true)
-        
-        cycle1.frame = CGRect(x: 50, y: 50, width: 100, height: 100)
-        cycle2.frame = CGRect(x: 50, y: 50, width: 100, height: 100)
-        
-        cycle1.set(starAngle: 0, endAngle: 180, animation: true)
-        cycle2.set(starAngle: 180, endAngle: 360, animation: true)
-        
-        mainView.addSubview(cycle1)
-        mainView.addSubview(cycle2)
-        
-        addRotateAnima()
-        createTimer()
-        
-    }
-    
-    private func createTimer() {
-        
-        timer = Timer(timeInterval: 3.0, target: self, selector: #selector(timeAction), userInfo: nil, repeats: true)
-        RunLoop.current.add(timer!, forMode: .defaultRunLoopMode)
-    }
-    
-    @objc fileprivate func timeAction() {
-        var largeD: CGFloat = 0
-        var smallD: CGFloat = 0
-        if type == .张开 {
-            largeD = 1
-            smallD = 0
-            type = .收缩
-        }else {
-            largeD = 0.5
-            smallD = 0.5
-            type = .张开
-        }
-        
-        cycle1.strokeEnd(value: largeD, duration: 3.0)
-        cycle2.strokeEnd(value: largeD, duration: 3.0)
-        
-        cycle1.strokeStart(value: smallD, duration: 3.0)
-        cycle2.strokeStart(value: smallD, duration: 3.0)
-        
-    }
 
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        timer?.fireDate = Date.distantFuture
+        addRotationAnima(view: mainView, duration: 2)
+        
+        sharp = createLoadingLayer(center: CGPoint(x: 50, y: 50), radius: 50, clockwise: true, width: 15)
+        sharp2 = createLoadingLayer(center: CGPoint(x: 50, y: 50), radius: 50, clockwise: false, width: 15)
+        
+        mainView.layer.addSublayer(sharp)
+        mainView.layer.addSublayer(sharp2)
+        
+        view.backgroundColor = UIColor.black
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        timer?.fireDate = Date.distantPast
+    private func createLoadingLayer(center: CGPoint, radius: CGFloat, clockwise: Bool, width: CGFloat = 3, color: UIColor = UIColor.white) -> CAShapeLayer {
+        
+        let path = UIBezierPath()
+        path.addArc(withCenter: center, radius: radius, startAngle: -0.5 * CGFloat.pi, endAngle: 0.5 * CGFloat.pi, clockwise: clockwise)
+        
+        
+        let layer = CAShapeLayer()
+        layer.path = path.cgPath
+        layer.lineCap = kCALineCapRound
+        layer.lineWidth = width
+        layer.fillColor = nil
+        layer.strokeColor = color.cgColor
+        
+        let anima1 = CABasicAnimation(keyPath: "strokeStart")
+        anima1.fromValue = 0.5
+        anima1.toValue = 0.1
+        anima1.duration = 2
+        anima1.repeatCount = HUGE
+        anima1.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+        anima1.autoreverses = true
+        
+        let anima2 = CABasicAnimation(keyPath: "strokeEnd")
+        anima2.fromValue = 0.5
+        anima2.toValue = 0.9
+        anima2.duration = 2
+        anima2.repeatCount = HUGE
+        anima2.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+        anima2.autoreverses = true
+        
+        layer.add(anima1, forKey: "")
+        layer.add(anima2, forKey: "")
+        return layer
     }
     
-    private func addRotateAnima() {
-    
-        let anima = CABasicAnimation(keyPath: "transform.rotation")
-        anima.fromValue = 0
-        anima.toValue = CGFloat.pi * 2
-        anima.duration = 3
-        anima.repeatCount = HUGE
-        cycle2.layer.add(anima, forKey: "rotation")
-        cycle1.layer.add(anima, forKey: "rotation")
+    private func addRotationAnima(view: UIView, duration: TimeInterval) {
+        let rotation = CABasicAnimation(keyPath: "transform.rotation")
+        rotation.fromValue = 0
+        rotation.toValue = 2 * CGFloat.pi
+        rotation.duration = duration
+        
+        rotation.repeatCount = HUGE
+        view.layer.add(rotation, forKey: "")
     }
-    
 }
